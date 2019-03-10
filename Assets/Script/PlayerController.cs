55a0f4dce314dc7d805cc8a4f43346a9d14d96fa
aw15 <didxodbs0987@naver.com>
2019-03-10 13:47:33 +0900

    TileMap Update & BasicGroundAttack

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,9 +8,8 @@ using UnityEngine;
 enum PlayerState
 {
     JUMP,
-    LEFTRUN,
-    RIGHTRUN,
-    GROUNDATTACK,
+    RUN,
+    BASIC_GROUNDATTACK,
     AIRATTACK
 }
 
@@ -54,12 +53,7 @@ public class PlayerController : MonoBehaviour
         KeyUpAction = new Dictionary<KeyCode, 행동>();
 
         KeyDownAction[KeyCode.Space] = Jump;
-        KeyDownAction[KeyCode.LeftArrow] = RunLeft;
-        KeyDownAction[KeyCode.RightArrow] = RunRight;
-
-
-        KeyUpAction[KeyCode.LeftArrow] = RunLeft;
-        KeyUpAction[KeyCode.RightArrow] = RunRight;
+        KeyDownAction[KeyCode.Q] = BasicGroundAttack;
 
 
 
@@ -67,10 +61,9 @@ public class PlayerController : MonoBehaviour
         state = new Dictionary<PlayerState, bool>();
         
 
-        state[PlayerState.RIGHTRUN] = false;
-        state[PlayerState.LEFTRUN] = false;
+        state[PlayerState.RUN] = false;
         state[PlayerState.JUMP] = false;
-        state[PlayerState.GROUNDATTACK] = false;
+        state[PlayerState.BASIC_GROUNDATTACK] = false;
         state[PlayerState.AIRATTACK] = false;
 
     }
@@ -90,14 +83,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         InputHandle();
- 
-        transform.Translate(moveForce * Time.deltaTime);
+        
 
         if (IsGrounded() && state[PlayerState.JUMP])
         {
             state[PlayerState.JUMP] = false;
             animator.SetBool("isJump", state[PlayerState.JUMP]);
         }
+
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("BasicGroundAttack") && !AnimatorIsPlaying())
+        {
+            state[PlayerState.BASIC_GROUNDATTACK] = false;
+            animator.SetBool("isBasicAttack", state[PlayerState.BASIC_GROUNDATTACK]);
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -111,12 +110,9 @@ public class PlayerController : MonoBehaviour
 
     private void InputHandle()
     {
-        //이동은 GetAxis를 이용해서 처리
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        if (moveInput > 0)
-            KeyDownAction[KeyCode.RightArrow](true);
-        else if(moveInput<0)
-            KeyDownAction[KeyCode.LeftArrow](true);
+
+        MoveInput();
+
 
 
         foreach (var data in KeyDownAction)
@@ -134,7 +130,7 @@ public class PlayerController : MonoBehaviour
 
     void Jump(bool isDown)
     {
-        if (!state[PlayerState.JUMP])
+        if (!state[PlayerState.JUMP] && isDown)
         {
             rigidbody.AddForce(new Vector2(0,speed.y), ForceMode2D.Impulse);
 
@@ -142,50 +138,64 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("isJump", state[PlayerState.JUMP]);
         }
     }
-    void RunLeft(bool isDown)
+
+    void BasicGroundAttack(bool isDown)
     {
-        if (isDown)
+        if (!state[PlayerState.JUMP] && isDown)
         {
-            moveForce.x = -speed.x;
-            spriteRenderer.flipX = true;
-            animator.SetBool("isRun",true);
-
-            state[PlayerState.LEFTRUN] = true;
-        }
-        else
-        {
-            moveForce.x = 0;
-            animator.SetBool("isRun", false);
-
-            state[PlayerState.LEFTRUN] = false;
+            state[PlayerState.BASIC_GROUNDATTACK] = true;
+            animator.SetBool("isBasicAttack", state[PlayerState.BASIC_GROUNDATTACK]);
+         
         }
     }
 
-    void RunRight(bool isDown)
+
+    void MoveInput()
     {
-        if (isDown)
+        //이동은 GetAxis를 이용해서 처리
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        if (moveInput > 0)
         {
             moveForce.x = speed.x;
             spriteRenderer.flipX = false;
             animator.SetBool("isRun", true);
 
-            state[PlayerState.RIGHTRUN] = true;
+            state[PlayerState.RUN] = true;
+        }
+        else if (moveInput < 0)
+        {
+            moveForce.x = -speed.x;
+            spriteRenderer.flipX = true;
+            animator.SetBool("isRun", true);
+
+            state[PlayerState.RUN] = true;
         }
         else
         {
             moveForce.x = 0;
             animator.SetBool("isRun", false);
-          
-            state[PlayerState.RIGHTRUN] = false;
-        }
-    }
 
+            state[PlayerState.RUN] = false;
+        }
+
+
+        transform.Translate(moveForce * Time.deltaTime);
+    }
 
     //땅에 있는지 확인.
     bool IsGrounded()
     {
         return bodyCollider.IsTouchingLayers(groundLayer)&&rigidbody.velocity.y<=0;
     }
+    //애니메이션이 플레이 중인지 확인.
+    bool AnimatorIsPlaying()
+    {
+        //return animator.GetCurrentAnimatorStateInfo(0).normalizedTime<0.95;
+
+        return animator.GetCurrentAnimatorStateInfo(0).length >
+       animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
 
     //상호작용 함수들
     public void GetDamaged(int damage,Vector3 position)
